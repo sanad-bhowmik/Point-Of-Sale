2a5098e3ad900eca70c3e7ff3314b17dd0bef653
sanad-bhowmik <sanad15-2952@diu.edu.bd>
2025-09-10 16:38:02 +0600

    Expense Posting Update

--- Modules/Expense/Http/Controllers/ExpenseController.php
+++ Modules/Expense/Http/Controllers/ExpenseController.php
@@ -13,36 +13,67 @@ use PhpOffice\PhpSpreadsheet\Calculation\MathTrig\Exp;
 class ExpenseController extends Controller
 {
 
-    public function index(ExpensesDataTable $dataTable) {
+    public function index(ExpensesDataTable $dataTable)
+    {
         abort_if(Gate::denies('access_expenses'), 403);
 
         return $dataTable->render('expense::expenses.index');
     }
 
 
-    public function create() {
+    public function create()
+    {
         abort_if(Gate::denies('create_expenses'), 403);
 
         return view('expense::expenses.create');
     }
 
-
-    public function store(Request $request) {
+    public function store(Request $request)
+    {
         abort_if(Gate::denies('create_expenses'), 403);
 
         $request->validate([
             'date' => 'required|date',
             'reference' => 'required|string|max:255',
+            'lc_id' => 'required|exists:lc,id',
             'category_id' => 'required',
             'amount' => 'required|numeric|max:2147483647',
-            'details' => 'nullable|string|max:1000'
+            'cf_agent_fee' => 'nullable|string|max:255',
+            'bl_verify' => 'nullable|string|max:255',
+            'shipping_charge' => 'nullable|string|max:255',
+            'port_bill' => 'nullable|string|max:255',
+            'labor_bill' => 'nullable|string|max:255',
+            'transport_bill' => 'nullable|string|max:255',
+            'other_receipt' => 'nullable|string|max:255',
+            'formalin_test' => 'nullable|string|max:255',
+            'radiation_cert' => 'nullable|string|max:255',
+            'labor_tips' => 'nullable|string|max:255',
+            'cf_commission' => 'nullable|string|max:255',
+            'ip_absence' => 'nullable|string|max:255',
+            'special_delivery' => 'nullable|string|max:255',
+            'details' => 'nullable|string|max:1000',
         ]);
 
         Expense::create([
             'date' => $request->date,
+            'reference' => $request->reference,
+            'lc_id' => $request->lc_id,
             'category_id' => $request->category_id,
             'amount' => $request->amount,
-            'details' => $request->details
+            'cf_agent_fee' => $request->cf_agent_fee,
+            'bl_verify' => $request->bl_verify,
+            'shipping_charge' => $request->shipping_charge,
+            'port_bill' => $request->port_bill,
+            'labor_bill' => $request->labor_bill,
+            'transport_bill' => $request->transport_bill,
+            'other_receipt' => $request->other_receipt,
+            'formalin_test' => $request->formalin_test,
+            'radiation_cert' => $request->radiation_cert,
+            'labor_tips' => $request->labor_tips,
+            'cf_commission' => $request->cf_commission,
+            'ip_absence' => $request->ip_absence,
+            'special_delivery' => $request->special_delivery,
+            'details' => $request->details,
         ]);
 
         toast('Expense Created!', 'success');
@@ -51,14 +82,16 @@ class ExpenseController extends Controller
     }
 
 
-    public function edit(Expense $expense) {
+    public function edit(Expense $expense)
+    {
         abort_if(Gate::denies('edit_expenses'), 403);
 
         return view('expense::expenses.edit', compact('expense'));
     }
 
 
-    public function update(Request $request, Expense $expense) {
+    public function update(Request $request, Expense $expense)
+    {
         abort_if(Gate::denies('edit_expenses'), 403);
 
         $request->validate([
@@ -83,7 +116,8 @@ class ExpenseController extends Controller
     }
 
 
-    public function destroy(Expense $expense) {
+    public function destroy(Expense $expense)
+    {
         abort_if(Gate::denies('delete_expenses'), 403);
 
         $expense->delete();
--- Modules/Expense/Resources/views/expenses/create.blade.php
+++ Modules/Expense/Resources/views/expenses/create.blade.php
@@ -3,89 +3,157 @@
 @section('title', 'Create Expense')
 
 @section('breadcrumb')
-    <ol class="breadcrumb border-0 m-0">
-        <li class="breadcrumb-item"><a href="{{ route('home') }}">Home</a></li>
-        <li class="breadcrumb-item"><a href="{{ route('expenses.index') }}">Expenses</a></li>
-        <li class="breadcrumb-item active">Add</li>
-    </ol>
+<ol class="breadcrumb border-0 m-0">
+    <li class="breadcrumb-item"><a href="{{ route('home') }}">Home</a></li>
+    <li class="breadcrumb-item"><a href="{{ route('expenses.index') }}">Expenses</a></li>
+    <li class="breadcrumb-item active">Add</li>
+</ol>
 @endsection
 
 @section('content')
-    <div class="container-fluid">
-        <form id="expense-form" action="{{ route('expenses.store') }}" method="POST">
-            @csrf
-            <div class="row">
-                <div class="col-lg-12">
-                    @include('utils.alerts')
-                    <div class="form-group">
-                        <button class="btn btn-primary">Create Expense <i class="bi bi-check"></i></button>
-                    </div>
+<div class="container-fluid">
+    <form id="expense-form" action="{{ route('expenses.store') }}" method="POST">
+        @csrf
+        <div class="row">
+            <div class="col-lg-12">
+                @include('utils.alerts')
+                <div class="form-group">
+                    <button class="btn btn-primary">Create Expense <i class="bi bi-check"></i></button>
                 </div>
-                <div class="col-lg-12">
-                    <div class="card">
-                        <div class="card-body">
-                            <div class="form-row">
-                                <div class="col-lg-6">
-                                    <div class="form-group">
-                                        <label for="reference">Reference <span class="text-danger">*</span></label>
-                                        <input type="text" class="form-control" name="reference" required readonly value="EXP">
-                                    </div>
-                                </div>
-                                <div class="col-lg-6">
-                                    <div class="form-group">
-                                        <label for="date">Date <span class="text-danger">*</span></label>
-                                        <input type="date" class="form-control" name="date" required value="{{ \Carbon\Carbon::now()->format('Y-m-d') }}">
-                                    </div>
+            </div>
+            <div class="col-lg-12">
+                <div class="card">
+                    <div class="card-body">
+                        <!-- Reference & LC -->
+                        <div class="form-row">
+                            <div class="col-lg-6">
+                                <div class="form-group">
+                                    <label for="reference">Reference <span class="text-danger">*</span></label>
+                                    <input type="text" class="form-control" name="reference" required readonly value="EXP">
                                 </div>
                             </div>
-
-                            <div class="form-row">
-                                <div class="col-lg-6">
-                                    <div class="form-group">
-                                        <label for="category_id">Category <span class="text-danger">*</span></label>
-                                        <select name="category_id" id="category_id" class="form-control" required>
-                                            <option value="" selected>Select Category</option>
-                                            @foreach(\Modules\Expense\Entities\ExpenseCategory::all() as $category)
-                                                <option value="{{ $category->id }}">{{ $category->category_name }}</option>
-                                            @endforeach
-                                        </select>
-                                    </div>
+                            <div class="col-lg-6">
+                                <div class="form-group">
+                                    <label for="lc_id">LC <span class="text-danger">*</span></label>
+                                    <select name="lc_id" id="lc_id" class="form-control select2" required>
+                                        <option value="" selected>Select LC</option>
+                                        @foreach(\App\Models\Lc::all() as $lc)
+                                        <option value="{{ $lc->id }}">{{ $lc->lc_name }}</option>
+                                        @endforeach
+                                    </select>
                                 </div>
-                                <div class="col-lg-6">
-                                    <div class="form-group">
-                                        <label for="amount">Amount <span class="text-danger">*</span></label>
-                                        <input id="amount" type="text" class="form-control" name="amount" required>
-                                    </div>
-                                </div>
-                            </div>
-
-                            <div class="form-group">
-                                <label for="details">Details</label>
-                                <textarea class="form-control" rows="6" name="details"></textarea>
                             </div>
                         </div>
+
+                        <!-- Date -->
+                        <div class="form-row">
+                            <div class="col-lg-6">
+                                <div class="form-group">
+                                    <label for="date">Date <span class="text-danger">*</span></label>
+                                    <input type="date" class="form-control" name="date" required value="{{ \Carbon\Carbon::now()->format('Y-m-d') }}">
+                                </div>
+                            </div>
+                            <div class="col-lg-6">
+                                <div class="form-group">
+                                    <label for="category_id">Category <span class="text-danger">*</span></label>
+                                    <select name="category_id" id="category_id" class="form-control" required>
+                                        <option value="" selected>Select Category</option>
+                                        @foreach(\Modules\Expense\Entities\ExpenseCategory::all() as $category)
+                                        <option value="{{ $category->id }}">{{ $category->category_name }}</option>
+                                        @endforeach
+                                    </select>
+                                </div>
+                            </div>
+                        </div>
+
+                        <!-- Amount -->
+                        <div class="form-row">
+                        </div>
+
+                        <!-- Other Fees and Details -->
+                        <div class="form-row">
+                            <div class="col-lg-6">
+                                <div class="form-group">
+                                    <label for="amount">Amount <span class="text-danger">*</span></label>
+                                    <input id="amount" type="text" class="form-control" name="amount" required>
+                                </div>
+                            </div>
+                            <div class="col-lg-6">
+                                <div class="form-group"><label for="cf_agent_fee">C&F Agent Fee</label><input type="text" class="form-control" name="cf_agent_fee"></div>
+                            </div>
+                            <div class="col-lg-6">
+                                <div class="form-group"><label for="bl_verify">B/L Verify</label><input type="text" class="form-control" name="bl_verify"></div>
+                            </div>
+                            <div class="col-lg-6">
+                                <div class="form-group"><label for="shipping_charge">Shipping/NOC Charge</label><input type="text" class="form-control" name="shipping_charge"></div>
+                            </div>
+                            <div class="col-lg-6">
+                                <div class="form-group"><label for="port_bill">Port Bill Charge</label><input type="text" class="form-control" name="port_bill"></div>
+                            </div>
+                            <div class="col-lg-6">
+                                <div class="form-group"><label for="labor_bill">Labor Bill</label><input type="text" class="form-control" name="labor_bill"></div>
+                            </div>
+                            <div class="col-lg-6">
+                                <div class="form-group"><label for="transport_bill">Transport/Survey Bill</label><input type="text" class="form-control" name="transport_bill"></div>
+                            </div>
+                            <div class="col-lg-6">
+                                <div class="form-group"><label for="other_receipt">Other Receipt Bill</label><input type="text" class="form-control" name="other_receipt"></div>
+                            </div>
+                            <div class="col-lg-6">
+                                <div class="form-group"><label for="formalin_test">Formalin Test Purpose</label><input type="text" class="form-control" name="formalin_test"></div>
+                            </div>
+                            <div class="col-lg-6">
+                                <div class="form-group"><label for="radiation_cert">Radiation Certificate</label><input type="text" class="form-control" name="radiation_cert"></div>
+                            </div>
+                            <div class="col-lg-6">
+                                <div class="form-group"><label for="labor_tips">Labor Tips</label><input type="text" class="form-control" name="labor_tips"></div>
+                            </div>
+                            <div class="col-lg-6">
+                                <div class="form-group"><label for="cf_commission">C&F Commission</label><input type="text" class="form-control" name="cf_commission"></div>
+                            </div>
+                            <div class="col-lg-6">
+                                <div class="form-group"><label for="ip_absence">IP Absence Fee</label><input type="text" class="form-control" name="ip_absence"></div>
+                            </div>
+                            <div class="col-lg-6">
+                                <div class="form-group"><label for="special_delivery">Special Delivery Fee</label><input type="text" class="form-control" name="special_delivery"></div>
+                            </div>
+                            <div class="col-lg-12">
+                                <div class="form-group"><label for="details">Details</label><textarea class="form-control" rows="6" name="details"></textarea></div>
+                            </div>
+                        </div>
+
                     </div>
                 </div>
             </div>
-        </form>
-    </div>
+        </div>
+    </form>
+</div>
 @endsection
 
 @push('page_scripts')
-    <script src="{{ asset('js/jquery-mask-money.js') }}"></script>
-    <script>
-        $(document).ready(function () {
-            $('#amount').maskMoney({
-                prefix:'{{ settings()->currency->symbol }}',
-                thousands:'{{ settings()->currency->thousand_separator }}',
-                decimal:'{{ settings()->currency->decimal_separator }}',
-            });
-
-            $('#expense-form').submit(function () {
-                var amount = $('#amount').maskMoney('unmasked')[0];
-                $('#amount').val(amount);
-            });
+<link href="https://cdn.jsdelivr.net/npm/select2@4.1.0-rc.0/dist/css/select2.min.css" rel="stylesheet" />
+<script src="https://cdn.jsdelivr.net/npm/select2@4.1.0-rc.0/dist/js/select2.min.js"></script>
+<script src="{{ asset('js/jquery-mask-money.js') }}"></script>
+<script>
+    $(document).ready(function() {
+        // Initialize Select2
+        $('#lc_id').select2({
+            placeholder: "Select LC",
+            allowClear: true,
+            width: '100%'
         });
-    </script>
-@endpush
 
+        // MaskMoney for amount
+        $('#amount').maskMoney({
+            prefix: '{{ settings()->currency->symbol }}',
+            thousands: '{{ settings()->currency->thousand_separator }}',
+            decimal: '{{ settings()->currency->decimal_separator }}',
+        });
+
+        $('#expense-form').submit(function() {
+            var amount = $('#amount').maskMoney('unmasked')[0];
+            $('#amount').val(amount);
+        });
+    });
+</script>
+@endpush
