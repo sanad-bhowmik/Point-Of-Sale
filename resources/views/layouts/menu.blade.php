a278024400d7ae92f51c9e7deeb608d7c65b6cbb
apurbo <apurbo.simt.2020@gmail.com>
2025-09-10 13:40:18 +0600

    updated

--- resources/views/layouts/menu.blade.php
+++ resources/views/layouts/menu.blade.php
@@ -74,71 +74,7 @@
             @endcan
         </ul>
     </li>
-    <li
-        class="c-sidebar-nav-item c-sidebar-nav-dropdown {{ request()->routeIs('products.*') || request()->routeIs('product-categories.*') ? 'c-show' : '' }}">
-        <a class="c-sidebar-nav-link c-sidebar-nav-dropdown-toggle" href="#">
-            <i class="c-sidebar-nav-icon bi bi-journal-bookmark" style="line-height: 1;"></i> Products
-        </a>
-        <ul class="c-sidebar-nav-dropdown-items">
-            @can('access_product_categories')
-                <li class="c-sidebar-nav-item">
-                    <a class="c-sidebar-nav-link {{ request()->routeIs('product-categories.*') ? 'c-active' : '' }}"
-                        href="{{ route('product-categories.index') }}">
-                        <i class="c-sidebar-nav-icon bi bi-collection" style="line-height: 1;"></i> Categories
-                    </a>
-                </li>
-            @endcan
-            @can('create_products')
-                <li class="c-sidebar-nav-item">
-                    <a class="c-sidebar-nav-link {{ request()->routeIs('products.create') ? 'c-active' : '' }}"
-                        href="{{ route('products.create') }}">
-                        <i class="c-sidebar-nav-icon bi bi-journal-plus" style="line-height: 1;"></i> Create Product
-                    </a>
-                </li>
-            @endcan
-            <li class="c-sidebar-nav-item">
-                <a class="c-sidebar-nav-link {{ request()->routeIs('products.index') ? 'c-active' : '' }}"
-                    href="{{ route('products.index') }}">
-                    <i class="c-sidebar-nav-icon bi bi-journals" style="line-height: 1;"></i> All Products
-                </a>
-            </li>
-            @can('print_barcodes')
-                <li class="c-sidebar-nav-item">
-                    <a class="c-sidebar-nav-link {{ request()->routeIs('barcode.print') ? 'c-active' : '' }}"
-                        href="{{ route('barcode.print') }}">
-                        <i class="c-sidebar-nav-icon bi bi-printer" style="line-height: 1;"></i> Print Barcode
-                    </a>
-                </li>
-            @endcan
-
-            @can('create_sizes')
-                <!-- Sizes Submenu -->
-                <li
-                    class="c-sidebar-nav-item c-sidebar-nav-dropdown {{ request()->routeIs('product.size.*') ? 'c-show' : '' }}">
-                    <a class="c-sidebar-nav-link c-sidebar-nav-dropdown-toggle" href="#">
-                        <i class="c-sidebar-nav-icon bi bi-box" style="line-height: 1;"></i> Sizes
-                    </a>
-                    <ul class="c-sidebar-nav-dropdown-items">
-                        <li class="c-sidebar-nav-item">
-                            <a class="c-sidebar-nav-link {{ request()->routeIs('product.size.create') ? 'c-active' : '' }}"
-                                href="{{ route('product.size.create') }}">
-                                Create Sizes
-                            </a>
-                        </li>
-                        <li class="c-sidebar-nav-item">
-                            <a class="c-sidebar-nav-link {{ request()->routeIs('product.size.view') ? 'c-active' : '' }}"
-                                href="{{ route('product.size.view') }}">
-                                View Sizes
-                            </a>
-                        </li>
-                    </ul>
-                </li>
-            @endcan
-
-        </ul>
-    </li>
 @endcan
-
 
 @can('access_adjustments')
     <li class="c-sidebar-nav-item c-sidebar-nav-dropdown {{ request()->routeIs('adjustments.*') ? 'c-show' : '' }}">
