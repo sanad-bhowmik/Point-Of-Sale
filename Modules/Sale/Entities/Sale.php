ec74a770d69c18108cfbe55e3b74931740de499f
sanad-bhowmik <sanad15-2952@diu.edu.bd>
2025-09-18 10:40:01 +0600

    09/18/2025

--- Modules/Sale/Entities/Sale.php
+++ Modules/Sale/Entities/Sale.php
@@ -11,18 +11,15 @@ class Sale extends Model
 
     protected $guarded = [];
 
-    public function saleDetails()
-    {
+    public function saleDetails() {
         return $this->hasMany(SaleDetails::class, 'sale_id', 'id');
     }
 
-    public function salePayments()
-    {
+    public function salePayments() {
         return $this->hasMany(SalePayment::class, 'sale_id', 'id');
     }
 
-    public static function boot()
-    {
+    public static function boot() {
         parent::boot();
 
         static::creating(function ($model) {
@@ -31,88 +28,31 @@ class Sale extends Model
         });
     }
 
-    public function scopeCompleted($query)
-    {
+    public function scopeCompleted($query) {
         return $query->where('status', 'Completed');
     }
 
-    /*
-    |--------------------------------------------------------------------------
-    | Accessors (Getters)
-    |--------------------------------------------------------------------------
-    */
-    public function getShippingAmountAttribute($value)
-    {
+    public function getShippingAmountAttribute($value) {
         return $value / 100;
     }
 
-    public function getPaidAmountAttribute($value)
-    {
+    public function getPaidAmountAttribute($value) {
         return $value / 100;
     }
 
-    public function getTotalAmountAttribute($value)
-    {
+    public function getTotalAmountAttribute($value) {
         return $value / 100;
     }
 
-    public function getDueAmountAttribute($value)
-    {
+    public function getDueAmountAttribute($value) {
         return $value / 100;
     }
 
-    public function getTaxAmountAttribute($value)
-    {
+    public function getTaxAmountAttribute($value) {
         return $value / 100;
     }
 
-    public function getDiscountAmountAttribute($value)
-    {
+    public function getDiscountAmountAttribute($value) {
         return $value / 100;
-    }
-
-    public function getPriceAttribute($value)
-    {
-        return $value / 100;
-    }
-
-    /*
-    |--------------------------------------------------------------------------
-    | Mutators (Setters)
-    |--------------------------------------------------------------------------
-    */
-    public function setShippingAmountAttribute($value)
-    {
-        $this->attributes['shipping_amount'] = $value * 100;
-    }
-
-    public function setPaidAmountAttribute($value)
-    {
-        $this->attributes['paid_amount'] = $value * 100;
-    }
-
-    public function setTotalAmountAttribute($value)
-    {
-        $this->attributes['total_amount'] = $value * 100;
-    }
-
-    public function setDueAmountAttribute($value)
-    {
-        $this->attributes['due_amount'] = $value * 100;
-    }
-
-    public function setTaxAmountAttribute($value)
-    {
-        $this->attributes['tax_amount'] = $value * 100;
-    }
-
-    public function setDiscountAmountAttribute($value)
-    {
-        $this->attributes['discount_amount'] = $value * 100;
-    }
-
-    public function setPriceAttribute($value)
-    {
-        $this->attributes['price'] = $value * 100;
     }
 }
